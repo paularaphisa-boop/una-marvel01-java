3a01f5ec3ca7f093f112a3c740eb746ef97822c8
paularaphisa-boop <paularaphisa@gmail.com>
2026-03-28 23:01:55 -0300

    Add files via upload

---
+++ ProjetoRenascimento.java
@@ -0,0 +1,19 @@
+public class ProjetoRenascimento {
+    public static void main(String[] args) {
+        boolean possuiCoragem = true;
+        int saudeFisica = 40;
+
+        System.out.println("--- Avaliação Estratégica Científica ---");
+
+        if (possuiCoragem && saudeFisica > 30) {
+            System.out.println("Resultado: Candidato ideal para o Soro.");
+            System.out.println("Status: Capitão América formado.");
+    }
+        else {
+            System.out.println("Resultado: Candidato Reprovado.");
+            System.out.println("Status: Permanecer em observação.");
+            
+        }
+    }
+    
+}
